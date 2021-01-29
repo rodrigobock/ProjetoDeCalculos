9238af8b4b4d9e8bb4fdfd6ed3e591b0ea78d8b0
rodrigobock <70343579+rodrigobock@users.noreply.github.com>
2021-01-29 13:43:19 -0300

    Atualização de código e implementação de tela

---
+++ ProjetoCalculos/F_Calculos.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class F_Calculos
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblMensagem = New System.Windows.Forms.Label()
+        Me.tb_num2 = New System.Windows.Forms.TextBox()
+        Me.tb_num1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BtnCalcular = New System.Windows.Forms.Button()
+        Me.BtnLimpar = New System.Windows.Forms.Button()
+        Me.BtnNovo = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft YaHei UI Light", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(103, 25)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(212, 30)
+        Me.Label3.TabIndex = 17
+        Me.Label3.Text = "Sistema de Cálculos"
+        '
+        'lblMensagem
+        '
+        Me.lblMensagem.AutoSize = True
+        Me.lblMensagem.Font = New System.Drawing.Font("Microsoft YaHei UI Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMensagem.Location = New System.Drawing.Point(17, 196)
+        Me.lblMensagem.Name = "lblMensagem"
+        Me.lblMensagem.Size = New System.Drawing.Size(67, 19)
+        Me.lblMensagem.TabIndex = 16
+        Me.lblMensagem.Text = "Resultado"
+        '
+        'tb_num2
+        '
+        Me.tb_num2.Font = New System.Drawing.Font("Microsoft YaHei UI Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tb_num2.Location = New System.Drawing.Point(95, 135)
+        Me.tb_num2.Name = "tb_num2"
+        Me.tb_num2.Size = New System.Drawing.Size(170, 25)
+        Me.tb_num2.TabIndex = 15
+        '
+        'tb_num1
+        '
+        Me.tb_num1.Font = New System.Drawing.Font("Microsoft YaHei UI Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tb_num1.Location = New System.Drawing.Point(95, 92)
+        Me.tb_num1.Name = "tb_num1"
+        Me.tb_num1.Size = New System.Drawing.Size(170, 25)
+        Me.tb_num1.TabIndex = 14
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft YaHei UI Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(21, 138)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 19)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Numero 2"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft YaHei UI Light", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(21, 92)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 19)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Numero 1"
+        '
+        'BtnCalcular
+        '
+        Me.BtnCalcular.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnCalcular.Font = New System.Drawing.Font("Microsoft YaHei UI Light", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnCalcular.Location = New System.Drawing.Point(281, 245)
+        Me.BtnCalcular.Name = "BtnCalcular"
+        Me.BtnCalcular.Size = New System.Drawing.Size(107, 41)
+        Me.BtnCalcular.TabIndex = 11
+        Me.BtnCalcular.Text = "Somar"
+        Me.BtnCalcular.UseVisualStyleBackColor = True
+        '
+        'BtnLimpar
+        '
+        Me.BtnLimpar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnLimpar.Font = New System.Drawing.Font("Microsoft YaHei UI Light", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnLimpar.Location = New System.Drawing.Point(148, 245)
+        Me.BtnLimpar.Name = "BtnLimpar"
+        Me.BtnLimpar.Size = New System.Drawing.Size(107, 41)
+        Me.BtnLimpar.TabIndex = 10
+        Me.BtnLimpar.Text = "Limpar"
+        Me.BtnLimpar.UseVisualStyleBackColor = True
+        '
+        'BtnNovo
+        '
+        Me.BtnNovo.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnNovo.Font = New System.Drawing.Font("Microsoft YaHei UI Light", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnNovo.Location = New System.Drawing.Point(21, 245)
+        Me.BtnNovo.Name = "BtnNovo"
+        Me.BtnNovo.Size = New System.Drawing.Size(107, 41)
+        Me.BtnNovo.TabIndex = 9
+        Me.BtnNovo.Text = "Novo"
+        Me.BtnNovo.UseVisualStyleBackColor = True
+        '
+        'F_Calculos
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(405, 292)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblMensagem)
+        Me.Controls.Add(Me.tb_num2)
+        Me.Controls.Add(Me.tb_num1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnCalcular)
+        Me.Controls.Add(Me.BtnLimpar)
+        Me.Controls.Add(Me.BtnNovo)
+        Me.Name = "F_Calculos"
+        Me.Text = "Sistema de Calculos"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblMensagem As Label
+    Friend WithEvents tb_num2 As TextBox
+    Friend WithEvents tb_num1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BtnCalcular As Button
+    Friend WithEvents BtnLimpar As Button
+    Friend WithEvents BtnNovo As Button
+End Class
---
+++ ProjetoCalculos/F_Calculos.vb
@@ -0,0 +1,58 @@
+﻿Public Class F_Calculos
+
+    Dim nomeTela As String
+
+    Private Sub F_Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        desabilitar()
+        lblMensagem.Text = ""
+    End Sub
+
+    Private Sub BtnNovo_Click(sender As Object, e As EventArgs) Handles BtnNovo.Click
+        tb_num1.Enabled = True
+        tb_num2.Enabled = True
+        BtnCalcular.Enabled = True
+        BtnLimpar.Enabled = True
+
+        tb_num1.Text = ""
+        tb_num2.Text = ""
+        lblMensagem.Text = ""
+    End Sub
+
+    Private Sub BtnLimpar_Click(sender As Object, e As EventArgs) Handles BtnLimpar.Click
+        tb_num1.Text = ""
+        tb_num2.Text = ""
+        lblMensagem.Text = ""
+
+        tb_num1.Focus()
+    End Sub
+
+    Private Sub BtnCalcular_Click(sender As Object, e As EventArgs) Handles BtnCalcular.Click
+        calcular()
+        desabilitar()
+    End Sub
+
+    Sub calcular()
+        Dim num1 As Integer
+        Dim num2 As Integer
+        Dim total As Integer
+
+        Try
+            num1 = tb_num1.Text
+            num2 = tb_num2.Text
+
+            total = num1 + num2
+
+            lblMensagem.Text = "Resultado: " & total
+        Catch ex As Exception
+            MsgBox("É necessário preencher os 2 campos corretamente. Erro: " & ex.Message)
+        End Try
+
+    End Sub
+
+    Sub desabilitar()
+        tb_num1.Enabled = False
+        tb_num2.Enabled = False
+        BtnCalcular.Enabled = False
+    End Sub
+
+End Class
